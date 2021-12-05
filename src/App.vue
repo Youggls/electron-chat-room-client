e4addb2dd4294119d6fa9b872fcd2c84d35943ba
Youggls <462046122@qq.com>
2021-12-05 14:36:29 +0800

    Update: Add login.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<login v-on:login-event="login"></login>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import dgram from 'dgram'
+import Login from './components/Login.vue'
+import {CRLF, LOGIN, LOGIN_SUCCESS, PROTOCOL_STRING} from './constants.js'
+import { message } from "ant-design-vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Login
   },
+  mounted() {
+    this.listenSocket = dgram.createSocket('udp4')
+    this.listenSocket.on('message', (msg, rinfo) => {
+      let decodedMsg = new TextDecoder(process.env.ENCODING).decode(msg)
+      decodedMsg = decodedMsg.replace(PROTOCOL_STRING, '')
+      const commandAndParams = decodedMsg.split(CRLF)
+      const command = commandAndParams[0]
+      const params = commandAndParams.slice(1)
+      if (command === LOGIN) {
+        this.handleLogin(params)
+      }
+      console.log(rinfo)
+    })    
+  },
+  methods: {
+    login(serverAddress, serverPort, userName) {
+      const msg = Buffer.from(PROTOCOL_STRING + LOGIN + CRLF + userName)
+      this.listenSocket.send(msg, serverPort, serverAddress, (err) => {
+        console.log(err)
+      })
+    },
+    handleLogin(params) {
+      console.log(params)
+      const status = params[0]
+      const errMsg = params[1]
+      if (status === LOGIN_SUCCESS) {
+        message.success(errMsg)
+      } else {
+        message.error(errMsg)
+      }
+    }
+  }
 }
 </script>
 
---
+++ src/components/Login.vue
@@ -0,0 +1,44 @@
+<template>
+  <a-form>
+    <a-form-item label="Server Address">
+      <a-input
+        type="text"
+        v-model:value="serverAddress"
+        placeholder="Server Address"
+      />
+    </a-form-item>
+    <a-form-item label="Server Port">
+      <a-input type="text" v-model:value="serverPort" placeholder="8989" />
+    </a-form-item>
+    <a-form-item label="User Name">
+      <a-input
+        type="text"
+        v-model:value="userName"
+        placeholder="Your User Name"
+      />
+    </a-form-item>
+  </a-form>
+  <a-button type="primary" v-on:click="loginButton()"> Connect </a-button>
+</template>
+
+<script>
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      serverAddress: "127.0.0.1",
+      serverPort: 8989,
+      userName: "Alice",
+    };
+  },
+  mounted() {
+  },
+  methods: {
+    loginButton() {
+      this.$emit('login-event', this.serverAddress, this.serverPort, this.userName)
+    },
+  },
+};
+</script>
+
