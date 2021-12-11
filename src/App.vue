6e53a17bf2fc7953e180c2b426e5ed53c8af77f8
Youggls <462046122@qq.com>
2021-12-11 21:30:52 +0800

    Update: send check command after login successfully.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-<login v-on:login-event="login"></login>
+  <login v-if="showLogin" v-on:login-event="login"></login>
 </template>
 
 <script>
 import dgram from 'dgram'
 import Login from './components/Login.vue'
-import {CRLF, LOGIN, LOGIN_SUCCESS, PROTOCOL_STRING} from './constants.js'
+import {CHECK, CRLF, LOGIN, PROTOCOL_STRING, TRUE} from './constants.js'
 import { message } from "ant-design-vue"
 
 export default {
@@ -13,35 +13,67 @@ export default {
   components: {
     Login
   },
+  data() {
+    return {
+      showLogin: true,
+      serverAddress: '',
+      serverPort: 0,
+      userName: '',
+    }    
+  },
   mounted() {
     this.listenSocket = dgram.createSocket('udp4')
     this.listenSocket.on('message', (msg, rinfo) => {
-      let decodedMsg = new TextDecoder(process.env.ENCODING).decode(msg)
-      decodedMsg = decodedMsg.replace(PROTOCOL_STRING, '')
-      const commandAndParams = decodedMsg.split(CRLF)
-      const command = commandAndParams[0]
-      const params = commandAndParams.slice(1)
+      const [command, params] = this.parseRawData(msg)
       if (command === LOGIN) {
         this.handleLogin(params)
+      } else if (command === CHECK) {
+        this.handleCheck(params)
       }
       console.log(rinfo)
-    })    
+    })
   },
   methods: {
     login(serverAddress, serverPort, userName) {
       const msg = Buffer.from(PROTOCOL_STRING + LOGIN + CRLF + userName)
+      this.serverAddress = serverAddress
+      this.serverPort = serverPort
+      this.userName = userName
       this.listenSocket.send(msg, serverPort, serverAddress, (err) => {
         console.log(err)
       })
     },
+    parseRawData(data) {
+      let decodedMsg = new TextDecoder(process.env.ENCODING).decode(data)
+      const [command, ...params] = decodedMsg.replace(PROTOCOL_STRING, '').split(CRLF)
+      return [command, params]
+    },
     handleLogin(params) {
-      console.log(params)
-      const status = params[0]
-      const errMsg = params[1]
-      if (status === LOGIN_SUCCESS) {
+      const [status, errMsg] = params
+      if (status === TRUE) {
         message.success(errMsg)
+        // Switch to chat room
+        this.showLogin = false
+        this.sendCheck()
       } else {
         message.error(errMsg)
+      }
+    },
+    sendCheck() {
+      const msg = Buffer.from(PROTOCOL_STRING + CHECK + CRLF + this.userName)
+      this.listenSocket.send(msg, this.serverPort, this.serverAddress, (err) => {
+        console.log(err)
+      })
+    },
+    handleCheck(params) {
+      const status = params[0]
+      if (status === TRUE) {
+        const [userNumber, userListStr] = params.slice(1)
+        const userList = userListStr.split('\n')
+        console.log('Current online users number: ', userNumber)
+        console.log('Current online users: ', userList)
+      } else {
+        message.error('You are offline!')
       }
     }
   }
